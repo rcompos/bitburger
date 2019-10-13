53f5b78b7cfcb6c06fda42182ff300d7f23e16aa
Ron Compos <rcompos@gmail.com>
2019-10-12 21:51:55 -0600

    Change repos storage dir location.

--- bb-sar.go
+++ bb-sar.go
@@ -16,7 +16,6 @@ import (
 
 //type RepositoriesOptions struct {
 //	Owner string `json:"owner"`
-//	Role  string `json:"role"` // role=[owner|admin|contributor|member]
 //}
 const BBURL = "https://bitbucket.org"
 
@@ -24,12 +23,10 @@ func main() {
 
 	rand.Seed(time.Now().UnixNano())
 
-	// Location under present working directory where repos are cloned
-	repodir := "repos"
-
 	bbUser := os.Getenv("BITBUCKET_USERNAME")
 	bbPassword := os.Getenv("BITBUCKET_PASSWORD")
-	//bbOwner := os.Getenv("BITBUCKET_OWNER")
+	bbOwner := os.Getenv("BITBUCKET_OWNER")
+	bbRole := os.Getenv("BITBUCKET_ROLE")
 	//bbReposlug := os.Getenv("BITBUCKET_REPOSLUG")
 
 	//fmt.Println("BITBUCKET_USERNAME:", bbUser)
@@ -37,48 +34,32 @@ func main() {
 	//fmt.Println("BITBUCKET_OWNER:", bbOwner)
 	//fmt.Println("BITBUCKET_REPOSLUG:", bbReposlug)
 
+	reposBaseDir := "repos"
+
+	opt := &bitbucket.RepositoriesOptions{}
+	opt.Owner = bbOwner
+	opt.Role = bbRole // [owner|admin|contributor|member]
+
 	c := bitbucket.NewBasicAuth(bbUser, bbPassword)
 
-	opt := &bitbucket.RepositoriesOptions{
-		//Owner: "solidfire",
-		Owner: "stumptown",
-		//Owner: "orangelo",
-		//Role: "member",
-		//Role: "admin",
-	}
-
-	/*
-		optRepository := &bitbucket.RepositoryOptions{
-			Owner: "solidfire",
-			//Owner: "stumptown",
-			//Owner: "orangelo",
-			RepoSlug: "kubespray-and-pray",
-		}
-	*/
-
-	//res, err := c.Repositories.ListForAccount(opt)
-
-	//var t *bitbucket.RepositoriesRes
-	//var err error
-	//t, err = c.Repositories.ListForAccount(opt)
-	t, err := c.Repositories.ListForAccount(opt)
-	//t, err := c.Repositories.ListForTeam(opt)
+	//repos, err := c.Repositories.ListForAccount(opt)
+	repos, err := c.Repositories.ListForAccount(opt)
 	if err != nil {
 		panic(err)
 	}
 
-	//fmt.Println("\nt:\n", t)
-	//fmt.Println("t Page:", t.Page)
-	//fmt.Println("t Size:", t.Size)
-	//fmt.Println("t MaxDepth:", t.MaxDepth)
-	//fmt.Println("t Pagelen:", t.Pagelen)
-	//fmt.Println("\nt Items:\n", t.Items)
+	//fmt.Println("\nt:\n", repos)
+	//fmt.Println("repos Page:", repos.Page)
+	//fmt.Println("repos Size:", repos.Size)
+	//fmt.Println("repos MaxDepth:", repos.MaxDepth)
+	//fmt.Println("repos Pagelen:", repos.Pagelen)
+	//fmt.Println("\nrepos Items:\n", repos.Items)
 
-	repositories := t.Items
+	repositories := repos.Items
 
 	//fmt.Println("\nrepositories:\n", repositories)
 
-	createRepoDir(repodir)
+	createRepoDir(fmt.Sprintf("%s/%s", reposBaseDir, bbOwner))
 
 	var repoList []string
 	//for k, v := range repositories {
@@ -94,73 +75,38 @@ func main() {
 	//fmt.Printf("Repos:\n")
 	for i, j := range repoList {
 		wg.Add(1) // <1>
-
-		fmt.Printf("%v> %v\n", i, j)
-
-		go func(i int, j string) {
+		//fmt.Printf("%v> %v\n", i, j)
+		go func(i int, j string, rdir string) {
 			defer wg.Done() // <2>
-			gitClone(j)
-			fmt.Printf("%vth goroutine sleeping...\n", i)
-			time.Sleep(2)
-		}(i, j)
+			errNum := gitClone(j, rdir)
+			if errNum == 128 {
+				gitPull(j)
+			}
+			fmt.Printf("%vth goroutine done.\n", i)
+			//time.Sleep(2)
+		}(i, j, reposBaseDir+"/"+bbOwner)
 	}
 
 	wg.Wait() // <3>
 	fmt.Println("All goroutines complete.")
 
-	//fmt.Println("repolist: ", repoList[0])
-
 	fmt.Println("Goodbye world!")
-
-	// Try to get Repository methods to run, such as Get
-	/*
-		//repoGot, err := c.Repositories.ListForAccount(optRepositories)
-		//repoGot, err := c.Repository.Get(optRepository)
-		repoGot, err := c.Repository.ListForks(optRepository)
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println()
-		fmt.Printf("repoGot:\n %v\n", repoGot)
-	*/
-
-	/*
-		//repos := t.(map[string]interface{})
-		repos := t.(map[string]interface{})
-		fmt.Printf("Repos:\n %v\n\bn", repos)
-
-		fmt.Printf("Page: %v\n", repos["page"])
-		fmt.Printf("Size: %v\n", repos["size"])
-		fmt.Printf("Pagelen: %v\n", repos["pagelen"])
-		fmt.Printf("Values:\n %v\n", repos["values"])
-
-		//valString := repos["values"]
-		valString := repos["values"].([]interface{})
-		fmt.Printf("\nvalString: %v\n", valString)
-		for k, v := range valString {
-			fmt.Printf("\n->  valString %v: %v\n", k, v)
-			fmt.Printf("\nName: %v\n", v.(map[string]interface{})["name"])
-			fmt.Printf("Links: %v\n", v.(map[string]interface{})["links"])
-		}
-	*/
 
 } //
 
-func gitClone(r string) {
-	//
+func gitClone(r string, rdir string) int {
 	// Git clone repository r
-	//
 
 	gitCloneString := fmt.Sprintf("git clone %s/%s", BBURL, r)
 	//gitCloneCmd := exec.Command("bash", "-c", gitCloneString)
 	//gitCloneCmd.Dir = "repos"
 	cmd := exec.Command("bash", "-c", gitCloneString)
-	cmd.Dir = "repos"
+	cmd.Dir = rdir
 	fmt.Println(cmd)
 	//cmd := exec.Command("ls", "-lah")
 	//err := gitCloneCmd.Run()
 
+	var errorNumber int = 0
 	var waitStatus syscall.WaitStatus
 	if err := cmd.Run(); err != nil {
 		if err != nil {
@@ -168,7 +114,7 @@ func gitClone(r string) {
 		}
 		if exitError, ok := err.(*exec.ExitError); ok {
 			waitStatus = exitError.Sys().(syscall.WaitStatus)
-			//errorNumber := waitStatus.ExitStatus()
+			errorNumber = waitStatus.ExitStatus()
 			////fmt.Printf("Output: %s\n", []byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
 			//fmt.Printf("Output: %s\n", []byte(fmt.Sprintf("%d", errorNumber)))
 		}
@@ -177,21 +123,60 @@ func gitClone(r string) {
 		waitStatus = cmd.ProcessState.Sys().(syscall.WaitStatus)
 		fmt.Printf("Output: %s\n", []byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
 	}
+	return errorNumber
 
 }
+
+func gitPull(r string) {
+	fmt.Println("git pull %s", r)
+}
+
+/*
+	// Git pull repository r
+
+	gitPullString := fmt.Sprintf("git pull")
+	//gitCloneCmd := exec.Command("bash", "-c", gitPullString)
+	//gitCloneCmd.Dir = "repos"
+	cmd := exec.Command("bash", "-c", gitPullString)
+	cmd.Dir = "repos"
+	fmt.Println(cmd)
+	//cmd := exec.Command("ls", "-lah")
+	//err := gitCloneCmd.Run()
+
+	var errorNumber int = 0
+	var waitStatus syscall.WaitStatus
+	if err := cmd.Run(); err != nil {
+		if err != nil {
+			//os.Stderr.WriteString(fmt.Sprintf("Error: %s\n", err.Error()))
+		}
+		if exitError, ok := err.(*exec.ExitError); ok {
+			waitStatus = exitError.Sys().(syscall.WaitStatus)
+			errorNumber = waitStatus.ExitStatus()
+			////fmt.Printf("Output: %s\n", []byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+			//fmt.Printf("Output: %s\n", []byte(fmt.Sprintf("%d", errorNumber)))
+		}
+	} else {
+		// Success
+		waitStatus = cmd.ProcessState.Sys().(syscall.WaitStatus)
+		fmt.Printf("Output: %s\n", []byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+	}
+	return errorNumber
+
+}
+*/
 
 func createRepoDir(repodir string) {
 	//
 	// Create repos directory repodir
 	//
 	//	if [ -d "repos" ]; then echo true; else echo false; fi
-	mkdirCmd := fmt.Sprintf("if [ ! -d %s ]; then mkdir -m775 %s; fi", repodir, repodir)
+	mkdirCmd := fmt.Sprintf("if [ ! -d %s ]; then mkdir -p -m775 %s; fi", repodir, repodir)
 	mkdirExec := exec.Command("bash", "-c", mkdirCmd)
 	mkdirExecOut, err := mkdirExec.Output()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(mkdirCmd)
-	fmt.Println(string(mkdirExecOut))
+	//fmt.Println(mkdirCmd)
+	fmt.Printf(string(mkdirExecOut))
 
 }
